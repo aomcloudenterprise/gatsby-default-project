c9fb1c1099b5e31590b288cf103b2186c3107f6a
danklut <danklutmeyer@hotmail.ca>
2018-06-27 04:45:00 -0700

    Create page-3.js

---
+++ src/pages/page-3.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+const SecondPage = () => (
+  <Layout>
+    <p>Page 3</p>
+    <h2></h2>
+    <Link to="/">Go back to the homepage</Link>
+  </Layout>
+)
+
+export default "/"
